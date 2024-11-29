0a0911d428d24b0933547375da18b0edc5b14be5
kakizoe.s <kaki.4261.zoes@gmail.com>
2024-11-30 04:54:13 +0900

    def post_isu_conditionのタイムアウト設定を修正

--- python/main.py
+++ python/main.py
@@ -738,10 +738,11 @@ def get_trend():
 def post_isu_condition(jia_isu_uuid):
     """ISUからのコンディションを受け取る"""
     # TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
-    drop_probability = 0.9
-    if random() <= drop_probability:
+    #drop_probability = 0.9
+    """if random() <= drop_probability:
         app.logger.warning("drop post isu condition request")
         return "", 202
+    """
     try:
         req = [PostIsuConditionRequest(**row) for row in request.json]
     except:
